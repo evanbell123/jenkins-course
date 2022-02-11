7c363a9f3d5f63cdd3925222a02ad55917f8c397
ebell2 <ebell2@wsgc.com>
2022-02-11 15:29:52 -0800

    change git url to https

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git@github.com:evanbell123/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/evanbell123/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('evanbell91@gmail.com')
         }
