31d9139230ed752eb6775e55ebcb8d8c0d72e9cc
ebell2 <ebell2@wsgc.com>
2022-02-11 15:54:09 -0800

    fix git url

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/evanbell123/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/evanbell123/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('evanbell91@gmail.com')
         }
